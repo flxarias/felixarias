f872a37353b8b234ecacf7dcc2c9b6db10592577
Félix Arias <41087627+flxarias@users.noreply.github.com>
2025-06-12 15:33:37 +0200

    Improve accessibility and SEO

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault();
-      document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-    });
-  });
-});
+
