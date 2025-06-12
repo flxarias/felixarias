6dd34a3b95ea1b39d8ba5a3fa3480daa353b4d18
Félix Arias <41087627+flxarias@users.noreply.github.com>
2025-06-12 14:41:04 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+});
